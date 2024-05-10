e75091e8014adff655dae2d00909f04a3878c068
[prabesh kharel] <[prabeshkharel168@gmail.com]>
2024-05-10 17:35:33 +0545

    day2 of javascript practice

---
+++ practice.js
@@ -0,0 +1,110 @@
+// var arr =[1,2,3,4,5,];
+
+// function testarr(array,num){
+//     array.push(num);
+//     return array.shift();
+// }
+
+// console.log(testarr(arr,6));
+// console.log(arr);
+
+
+// function searchfriend(name){
+// var result="";
+// var myFriends ={
+
+//     "frind1" : "ayush",
+//     "friend2" : "aryan",
+//     "friend3": "biplop"
+
+
+// } ;
+// return result = myFriends[name]
+// // var handsome1= myFriends.frind1; 
+// // var handsome2 = myFriends.frind2; 
+// // var handsome3 = myFriends.frind3; 
+// }
+// console.log(searchfriend("friend2"));
+
+// var myFriends ={
+    
+//     "frind1" : "ayush",
+//     "friend2" : "aryan",
+//     "friend3": "biplop",
+// };
+
+// function searchfriend(name){
+//     if(myFriends.hasOwnProperty(name)){
+//         return myFriends[name]
+//     }else{
+// return "not Found"
+//     }
+    
+
+    
+    // }
+    // console.log(searchfriend("friend4"));
+
+
+
+// var bikes= [
+// {
+// "Company": "yamaha",
+// "list":["mt-15","FZ","RX"]
+
+// } ,
+// {
+//     "Company" : "bajaj",
+//     "list":  ["pulsar","dominar","avenger"]
+// }
+// ]
+// console.log(bikes[0].list[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var Numbers=[[1,2,3],[8,9],[4,6]];
+// function multiply(Array){
+
+// var  product = 1;
+
+
+// for(i=0;i<Array.length;i++){
+//     for(j=0;j<Array[i].length;j++){
+//         product *= Array[i][j];
+//     }
+// }
+// return product;
+// }
+// console.log(multiply(Numbers));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
